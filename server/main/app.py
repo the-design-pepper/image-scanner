74f57ef7031763079ea591513909d9a96f37b32c
kkadakia <kkadakia@thebulldoggroupllc.com>
2019-04-11 15:04:32 -0400

    able to find egde, contour and transform image

---
+++ server/main/app.py
@@ -0,0 +1,136 @@
+from flask import Flask
+import numpy as np
+import argparse
+import cv2
+from skimage.filters import threshold_local
+import imutils
+
+app = Flask(__name__)
+
+
+def order_points(pts):
+    # initialize a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-let
+    # x-coordinates or the top-right and top-left x-coordinates
+    width_a = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    width_b = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    max_width = max(int(width_a), int(width_b))
+
+    # compute the height of the new images, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    height_a = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    height_b = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    max_height = max(int(height_a), int(height_b))
+
+    dst = np.array([
+        [0, 0],
+        [max_width - 1, 0],
+        [max_width - 1, max_height - 1],
+        [0, max_height - 1]
+    ], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (max_width, max_height))
+
+    return warped
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image to be scanned")
+args = vars(ap.parse_args())
+
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it
+_image = cv2.imread(args['image'])
+ratio = _image.shape[0] / 500.0
+orig = _image.copy()
+_image = imutils.resize(_image, height=500)
+
+# convert the image to gray scale, blur it, and find edges
+# in the image
+gray = cv2.cvtColor(_image, cv2.COLOR_BGR2RGB)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+# show the original image and the edge detected image
+# print("STEP 1: Edge Detection")
+# cv2.imshow("Image", _image)
+# cv2.imshow("Edged", edged)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+screenCnt = ''
+# loop over the contours
+for c in cnts:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    # if our approximated contour has four points, then we
+    # can assume that we have found our screen
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+# show the contour (outline) of the piece of paper
+# print("STEP 2: Find contours of paper")
+# cv2.drawContours(_image, [screenCnt], -1, (0, 255, 0), 2)
+# cv2.imshow("Outline", _image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# apply the four point transform to obtain a top-down
+# view of the original image
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+# convert the warped image to grayscale, then threshold it
+# to give it that 'black and white' paper effect
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset=10, method="gaussian")
+warped = (warped > T).astype("uint8") * 255
+
+# show the original and scanned images
+print("STEP 3: Apply perspective transform")
+cv2.imshow("Original", imutils.resize(orig, height=650))
+cv2.imshow("Scanned", imutils.resize(warped, height=650))
+cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
